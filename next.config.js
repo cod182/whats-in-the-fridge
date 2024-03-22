d4435a1767ba000a6ed349f6aaa3178187dec593
Codie Stephens-Evans <codie18@gmail.com>
2024-03-22 20:28:58 +0000

    next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,28 @@
 /** @type {import('next').NextConfig} */
+
+const headers = [
+  "Accept", "Accept-Version", "Content-Length",
+  "Content-MD5", "Content-Type", "Date", "X-Api-Version",
+  "X-CSRF-Token", "X-Requested-With",
+];
+
+
 const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+  reactStrictMode: true,
+  async headers() {
+    return [{
+      source: "/api/(.*)",
+      headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: 'Access-Control-Allow-Origin', value: `${process.env.NEXTAUTH_URL}` },
+        { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT" },
+        { key: "Access-Control-Allow-Headers", value: headers.join(", ") }
+      ]
+    }];
+  }
 }
 
 module.exports = nextConfig
