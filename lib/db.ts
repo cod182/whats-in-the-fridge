d4d05fcb9d4c1f3b802b47c5caaa5781a82341f3
Codie Stephens-Evans <codie18@gmail.com>
2024-03-23 16:24:39 +0000

    changed from serverless-mysql to mysql2

--- lib/db.ts
+++ lib/db.ts
@@ -1,21 +1,46 @@
-import mysql from 'serverless-mysql';
-const db = mysql({
-  config: {
-    host: process.env.NEXT_DB_HOST,
-    user: process.env.NEXT_DB_USER,
-    password: process.env.NEXT_DB_PASS,
-    database: process.env.NEXT_DB_DBNAME,
-    port: parseInt(process.env.NEXT_DB_PORT || '3306', 10),
-  }
+// import mysql from 'serverless-mysql';
+// const db = mysql({
+//   config: {
+//     host: process.env.NEXT_DB_HOST,
+//     user: process.env.NEXT_DB_USER,
+//     password: process.env.NEXT_DB_PASS,
+//     database: process.env.NEXT_DB_DBNAME,
+//     port: parseInt(process.env.NEXT_DB_PORT || '3306', 10),
+//   }
+// });
+
+// export const executeQuery = async (query: string, values: any = []) => {
+//   try {
+//     const results = await db.query(query, values);
+//     await db.end();
+//     return results;
+//   } catch (error) {
+//     return { error };
+//   }
+
+// }
+
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+  host: process.env.NEXT_DB_HOST,
+  user: process.env.NEXT_DB_USER,
+  password: process.env.NEXT_DB_PASS,
+  database: process.env.NEXT_DB_DBNAME,
+  port: parseInt(process.env.NEXT_DB_PORT || '3306', 10),
 });
 
-export const executeQuery = async (query: string, values: any = []) => {
+export const executeQuery = async (query: string, values: any[] = []) => {
+  let connection;
   try {
-    const results = await db.query(query, values);
-    await db.end();
-    return results;
+    connection = await pool.getConnection();
+    const [rows] = await connection.query(query, values);
+    return rows;
   } catch (error) {
     return { error };
+  } finally {
+    if (connection) {
+      connection.release();
+    }
   }
-
-}
+};
