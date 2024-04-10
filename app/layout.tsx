a143587e854f047e5f3e4e10321b48e5bdd0db78
Codie Stephens-Evans <codie18@gmail.com>
2024-04-10 15:11:18 +0100

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 
 import { Nav, Provider } from '@/components';
 
+import { Analytics } from "@vercel/analytics/react"
 import Footer from '@/components/Footer/Footer';
 import type { Metadata } from 'next';
 
@@ -20,26 +21,29 @@ export default async function RootLayout({
 
 
   return (
-    <html lang="en">
-      <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="apple-mobile-web-app-title" content="What's In The Fridge?" />
-        <meta name="application-name" content="What's In The Fridge?" />
-        <meta name="msapplication-TileColor" content="#2b5797" />
-        <meta name="theme-color" content="#ffffff" />
-      </head>
+    <>
+      <html lang="en">
+        <head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="apple-mobile-web-app-title" content="What's In The Fridge?" />
+          <meta name="application-name" content="What's In The Fridge?" />
+          <meta name="msapplication-TileColor" content="#2b5797" />
+          <meta name="theme-color" content="#ffffff" />
+        </head>
 
-      <body className="max-w-6xl mx-auto bg-[#88caff] min-h-[100vh] flex flex-col">
-        <Provider>
-          <Nav />
-          {children}
-        </Provider>
-        <Footer />
-      </body>
-    </html >
+        <body className="max-w-6xl mx-auto bg-[#88caff] min-h-[100vh] flex flex-col">
+          <Provider>
+            <Nav />
+            {children}
+          </Provider>
+          <Footer />
+        </body>
+      </html >
+      <Analytics />
+    </>
   );
 }
