b14cc55344fbaede937e53f5f1910a96b2c282d4
Codie Stephens-Evans <codie18@gmail.com>
2024-04-26 15:22:54 +0100

    finished the style for item search

--- components/IconSearch/IconSearch.tsx
+++ components/IconSearch/IconSearch.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import FadeInHOC from '../FadeInHOC/FadeInHOC';
 import Image from 'next/image';
 import { customImages } from '@/static/custom-item-images';
 import { icons } from '../../static/icons';
@@ -36,28 +37,30 @@ const IconSearch = () => {
       <div className='my-2'>
         {/* Icon Selection */}
 
-        <p className='my-2'>Common Icons:</p>
-        <div className='grid grid-cols-1 gap-2 xxxs:grid-cols-2 xxs:grid-cols-3 xs:grid-cols-4 sm:grid-cols-5 md:grid-cols-6 border-[1px] border-gray-400 rounded-md p-2'>
+        <p className='my-2 font-semibold'>Common Icons:</p>
+        <div className='grid grid-cols-1 gap-2 xxxs:grid-cols-2 xxs:grid-cols-3 xs:grid-cols-4 sm:grid-cols-5 md:grid-cols-6 border-[1px] border-gray-400 rounded-md p-2 bg-blue-700/50'>
           {customImages.map((commonIcon, index) => (
-            <label key={index} className='flex items-center w-full mx-auto transition-all duration-200 active:scale-110 ease'>
-              <input
-                type="radio"
-                name="itemIcon"
-                value={commonIcon.icon}
-                onChange={(e) => setSelectedIcon(e.target.value)}
-                className='hidden mr-2 group'
-                checked={selectedIcon === commonIcon.icon}
-              />
-              <div className="flex flex-col items-center justify-center mx-auto">
-                <Image
-                  src={`/assets/images/items/${commonIcon.icon}`}
-                  alt={commonIcon.name}
-                  className={`mx-auto hover:scale-110 cursor-pointer transition-all duration-200 ease ${selectedIcon === commonIcon.icon ? 'rounded-full scale-110 border-[2px] shadow-xl border-green-400' : ''} `}
-                  width={50}
-                  height={50}
+            <FadeInHOC key={commonIcon.name.replace(/\s/g, "_").toLowerCase()} delayNumber={index * 100} direction='up'>
+              <label className='flex items-center w-full mx-auto transition-all duration-200 active:scale-110 ease'>
+                <input
+                  type="radio"
+                  name="itemIcon"
+                  value={commonIcon.icon}
+                  onChange={(e) => setSelectedIcon(e.target.value)}
+                  className='hidden mr-2 group'
+                  checked={selectedIcon === commonIcon.icon}
                 />
-              </div>
-            </label>
+                <div className="flex flex-col items-center justify-center mx-auto">
+                  <Image
+                    src={`/assets/images/items/${commonIcon.icon}`}
+                    alt={commonIcon.name.replace(/\s/g, "_").toLowerCase()}
+                    className={`mx-auto hover:scale-110 cursor-pointer transition-all duration-200 ease ${selectedIcon === commonIcon.icon ? 'rounded-full scale-110 border-[2px] shadow-xl border-green-400' : ''} `}
+                    width={50}
+                    height={50}
+                  />
+                </div>
+              </label>
+            </FadeInHOC>
           ))}
 
         </div>
@@ -67,39 +70,41 @@ const IconSearch = () => {
 
       {/* START All Icon Search */}
       <div className=''>
-        <p className='my-2'>Search for an Icon:</p>
+        <p className='my-2 font-semibold'>Search for an Icon:</p>
 
-        <div>
-          <input id='itemSearch' type="text" className='rounded-md border-[1px] border-black px-4 text-gray-500 h-[40px] sm:h-[30px] w-full' placeholder='Search for an item' onChange={(e) => handleIconSearch(e.target.value)} />
+        <div className='my-2'>
+          <input id='itemSearch' type="text" className='rounded-md border-[1px] border-black px-4 text-gray-500 h-[40px] sm:h-[30px] w-full xxs:w-[200px]' placeholder='E.g. Melon' onChange={(e) => handleIconSearch(e.target.value)} />
         </div>
         <div
-          className={`gap-2 w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 ${searchResults && searchResults.length > 0 ? 'max-h-[300px] overflow-scroll' : searchTerm != '' && searchTerm != undefined ? 'max-h-[300px] overflow-scroll' : 'max-h-[0px] overflow-hidden'}`}
+          className={`gap-2 w-full grid grid-cols-1 xxxs:grid-cols-2 xxs:grid-cols-3 xs:grid-cols-4 sm:grid-cols-5 md:grid-cols-6 border-[1px] border-gray-400 rounded-md px-2 bg-blue-700/50 transition-all duration-200 ease ${searchResults && searchResults.length > 0 ? 'max-h-[300px] overflow-scroll py-2' : searchTerm != '' && searchTerm != undefined ? 'max-h-[300px] overflow-scroll py-2' : 'py-0 max-h-[0px] overflow-hidden'}`}
         >
           {searchResults && searchResults.length > 0 ?
-            searchResults?.map((icon) => (
-              <label key={icon.name} className='flex items-center w-full mx-auto transition-all duration-200 active:scale-110 ease'>
-                <input
-                  type="radio"
-                  name="itemIcon"
-                  value={icon.icon}
-                  onChange={(e) => setSelectedIcon(e.target.value)}
-                  className='hidden mr-2 group'
-                  checked={selectedIcon === icon.icon}
-                />
-                <div className="flex flex-col items-center justify-center mx-auto">
-                  <Image
-                    src={`/assets/images/items/${icon.icon}`}
-                    alt={icon.name}
-                    className={`mx-auto hover:scale-110 cursor-pointer transition-all duration-200 ease ${selectedIcon === icon.icon ? 'rounded-full scale-110 border-[2px] shadow-xl border-green-400' : ''} `}
-                    width={50}
-                    height={50}
+            searchResults?.map((icon, index) => (
+              <FadeInHOC key={icon.name.replace(/\s/g, "_").toLowerCase()} delayNumber={index * 100} direction='up'>
+                <label className='flex items-center w-full mx-auto transition-all duration-200 active:scale-110 ease'>
+                  <input
+                    type="radio"
+                    name="itemIcon"
+                    value={icon.icon}
+                    onChange={(e) => setSelectedIcon(e.target.value)}
+                    className='hidden mr-2 group'
+                    checked={selectedIcon === icon.icon}
                   />
-                </div>
-              </label>
+                  <div className="flex flex-col items-center justify-center mx-auto">
+                    <Image
+                      src={`/assets/images/items/${icon.icon}`}
+                      alt={icon.name.replace(/\s/g, "_").toLowerCase()}
+                      className={`mx-auto hover:scale-110 cursor-pointer transition-all duration-200 ease ${selectedIcon === icon.icon ? 'rounded-full scale-110 border-[2px] shadow-xl border-green-400' : ''} `}
+                      width={50}
+                      height={50}
+                    />
+                  </div>
+                </label>
+              </FadeInHOC>
             ))
             :
             (
-              <div className='w-full'>
+              <div className='w-ful font-semibold'>
                 <p>No Icons Found</p>
               </div>
             )}
