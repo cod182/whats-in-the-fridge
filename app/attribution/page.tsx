42c99a8f5a3e951b54b46a476c533a40f8e1fc30
Codie Stephens-Evans <codie18@gmail.com>
2024-05-07 01:47:39 +0100

    added more icons

--- app/attribution/page.tsx
+++ app/attribution/page.tsx
@@ -21,6 +21,7 @@ const page = () => {
         <p className='hover:text-blue-700'>Defrost by kystudio from <a href="https://thenounproject.com/browse/icons/term/defrost/" target="_blank" title="Defrost Icons">Noun Project</a> (CC BY 3.0)</p>
         <p className='hover:text-blue-700'>Food by Maxie from <a href="https://thenounproject.com/browse/icons/term/food/" target="_blank" title="Food Icons">Noun Project</a> (CC BY 3.0)</p>
 
+        <h3 className='font-bold underline ml-2'>Vegatables</h3>
         Artichoke by Bstn Stnhr from <a href="https://thenounproject.com/browse/icons/term/artichoke/" target="_blank" title="Artichoke Icons">Noun Project</a> (CC BY 3.0)
         aubergine by Georgiana Ionescu from <a href="https://thenounproject.com/browse/icons/term/aubergine/" target="_blank" title="aubergine Icons">Noun Project</a> (CC BY 3.0)
         Asparagus by Icons Producer from <a href="https://thenounproject.com/browse/icons/term/asparagus/" target="_blank" title="Asparagus Icons">Noun Project</a> (CC BY 3.0)
@@ -57,7 +58,52 @@ const page = () => {
         Lima beans by Amethyst Studio from <a href="https://thenounproject.com/browse/icons/term/lima-beans/" target="_blank" title="Lima beans Icons">Noun Project</a> (CC BY 3.0)
         mung beans by nuengrutai from <a href="https://thenounproject.com/browse/icons/term/mung-beans/" target="_blank" title="mung beans Icons">Noun Project</a> (CC BY 3.0)
         peanuts by Kartik Malhotra from <a href="https://thenounproject.com/browse/icons/term/peanuts/" target="_blank" title="peanuts Icons">Noun Project</a> (CC BY 3.0)
+        beans by Agajah Mada from <a href="https://thenounproject.com/browse/icons/term/beans/" target="_blank" title="beans Icons">Noun Project</a> (CC BY 3.0)
+        peas by Giorgi from <a href="https://thenounproject.com/browse/icons/term/peas/" target="_blank" title="peas Icons">Noun Project</a> (CC BY 3.0)
+        Mushrooms by rendicon from <a href="https://thenounproject.com/browse/icons/term/mushrooms/" target="_blank" title="Mushrooms Icons">Noun Project</a> (CC BY 3.0)
+        chives by Gruffy Studio from <a href="https://thenounproject.com/browse/icons/term/chives/" target="_blank" title="chives Icons">Noun Project</a> (CC BY 3.0)
+        garlic gourmet by CHARIE Tristan from <a href="https://thenounproject.com/browse/icons/term/garlic-gourmet/" target="_blank" title="garlic gourmet Icons">Noun Project</a> (CC BY 3.0)
+        Leek by Icogenix from <a href="https://thenounproject.com/browse/icons/term/leek/" target="_blank" title="Leek Icons">Noun Project</a> (CC BY 3.0)
+        onion by farra nugraha from <a href="https://thenounproject.com/browse/icons/term/onion/" target="_blank" title="onion Icons">Noun Project</a> (CC BY 3.0)
+        shallot by Enamo Studios from <a href="https://thenounproject.com/browse/icons/term/shallot/" target="_blank" title="shallot Icons">Noun Project</a> (CC BY 3.0)
+        spring onion by Icogenix from <a href="https://thenounproject.com/browse/icons/term/spring-onion/" target="_blank" title="spring onion Icons">Noun Project</a> (CC BY 3.0)
+        Capsicum by Icongeek26 from <a href="https://thenounproject.com/browse/icons/term/capsicum/" target="_blank" title="Capsicum Icons">Noun Project</a> (CC BY 3.0)
+        Jalapeno by Ranah Pixel Studio from <a href="https://thenounproject.com/browse/icons/term/jalapeno/" target="_blank" title="Jalapeno Icons">Noun Project</a> (CC BY 3.0)
+        Pepper by Scott Witthoft from <a href="https://thenounproject.com/browse/icons/term/pepper/" target="_blank" title="Pepper Icons">Noun Project</a> (CC BY 3.0)
+        Paprika by Gendis Studio from <a href="https://thenounproject.com/browse/icons/term/paprika/" target="_blank" title="Paprika Icons">Noun Project</a> (CC BY 3.0)
+        Chile by Adrien Coquet from <a href="https://thenounproject.com/browse/icons/term/chile/" target="_blank" title="Chile Icons">Noun Project</a> (CC BY 3.0)
+        cayenne pepper by Lima Studio from <a href="https://thenounproject.com/browse/icons/term/cayenne-pepper/" target="_blank" title="cayenne pepper Icons">Noun Project</a> (CC BY 3.0)
+        radicchio by Victoruler from <a href="https://thenounproject.com/browse/icons/term/radicchio/" target="_blank" title="radicchio Icons">Noun Project</a> (CC BY 3.0)
+        Rhubarb by Supanut Piyakanont from <a href="https://thenounproject.com/browse/icons/term/rhubarb/" target="_blank" title="Rhubarb Icons">Noun Project</a> (CC BY 3.0)
+        Beetroot by Gofficon from <a href="https://thenounproject.com/browse/icons/term/beetroot/" target="_blank" title="Beetroot Icons">Noun Project</a> (CC BY 3.0)
+        Carrot by jisangbong from <a href="https://thenounproject.com/browse/icons/term/carrot/" target="_blank" title="Carrot Icons">Noun Project</a> (CC BY 3.0)
+        celeriac by Azam Ishaq from <a href="https://thenounproject.com/browse/icons/term/celeriac/" target="_blank" title="celeriac Icons">Noun Project</a> (CC BY 3.0)
+        eddoe by Amethyst Studio from <a href="https://thenounproject.com/browse/icons/term/eddoe/" target="_blank" title="eddoe Icons">Noun Project</a> (CC BY 3.0)
+        konjac by Amethyst Studio from <a href="https://thenounproject.com/browse/icons/term/konjac/" target="_blank" title="konjac Icons">Noun Project</a> (CC BY 3.0)
+        taro by Supanut Piyakanont from <a href="https://thenounproject.com/browse/icons/term/taro/" target="_blank" title="taro Icons">Noun Project</a> (CC BY 3.0)
+        water chestnut by Lars Meiertoberens from <a href="https://thenounproject.com/browse/icons/term/water-chestnut/" target="_blank" title="water chestnut Icons">Noun Project</a> (CC BY 3.0)
+        ginger by iconcheese from <a href="https://thenounproject.com/browse/icons/term/ginger/" target="_blank" title="ginger Icons">Noun Project</a> (CC BY 3.0)
+        parsnip by Kmg Design from <a href="https://thenounproject.com/browse/icons/term/parsnip/" target="_blank" title="parsnip Icons">Noun Project</a> (CC BY 3.0)
+        swede by Tom Fricker from <a href="https://thenounproject.com/browse/icons/term/swede/" target="_blank" title="swede Icons">Noun Project</a> (CC BY 3.0)
+        wasabi by Kmg Design from <a href="https://thenounproject.com/browse/icons/term/wasabi/" target="_blank" title="wasabi Icons">Noun Project</a> (CC BY 3.0)
+        horseradish by Pronto Illustration from <a href="https://thenounproject.com/browse/icons/term/horseradish/" target="_blank" title="horseradish Icons">Noun Project</a> (CC BY 3.0)
+        white radish by murmur from <a href="https://thenounproject.com/browse/icons/term/white-radish/" target="_blank" title="white radish Icons">Noun Project</a> (CC BY 3.0)
+        Turnip by Muna Fvck from <a href="https://thenounproject.com/browse/icons/term/turnip/" target="_blank" title="Turnip Icons">Noun Project</a> (CC BY 3.0)
+        jicama by Side Project from <a href="https://thenounproject.com/browse/icons/term/jicama/" target="_blank" title="jicama Icons">Noun Project</a> (CC BY 3.0)
+        potato by izzul fikry from <a href="https://thenounproject.com/browse/icons/term/potato/" target="_blank" title="potato Icons">Noun Project</a> (CC BY 3.0)
+        yam by T. Kiefer Robertson from <a href="https://thenounproject.com/browse/icons/term/yam/" target="_blank" title="yam Icons">Noun Project</a> (CC BY 3.0)
+        black salsify by Side Project from <a href="https://thenounproject.com/browse/icons/term/black-salsify/" target="_blank" title="black salsify Icons">Noun Project</a> (CC BY 3.0)
+        Agriculture by Colourcreatype from <a href="https://thenounproject.com/browse/icons/term/agriculture/" target="_blank" title="Agriculture Icons">Noun Project</a> (CC BY 3.0)
+        Squash by Gofficon from <a href="https://thenounproject.com/browse/icons/term/squash/" target="_blank" title="Squash Icons">Noun Project</a> (CC BY 3.0)
+        bitter melon by asembagus-art from <a href="https://thenounproject.com/browse/icons/term/bitter-melon/" target="_blank" title="bitter melon Icons">Noun Project</a> (CC BY 3.0)
+        courgette by Mary Bowie from <a href="https://thenounproject.com/browse/icons/term/courgette/" target="_blank" title="courgette Icons">Noun Project</a> (CC BY 3.0)
+        Cucumber by dedi prasetyo from <a href="https://thenounproject.com/browse/icons/term/cucumber/" target="_blank" title="Cucumber Icons">Noun Project</a> (CC BY 3.0)
+        delicata by Amethyst Studio from <a href="https://thenounproject.com/browse/icons/term/delicata/" target="_blank" title="delicata Icons">Noun Project</a> (CC BY 3.0)
+        marrow by Phạm Thanh Lộc from <a href="https://thenounproject.com/browse/icons/term/marrow/" target="_blank" title="marrow Icons">Noun Project</a> (CC BY 3.0)
+        Fruit by Renee Ramsey-Passmore from <a href="https://thenounproject.com/browse/icons/term/fruit/" target="_blank" title="Fruit Icons">Noun Project</a> (CC BY 3.0)
+        <hr />
 
+        <h3 className='font-bold underline ml-2'>Next</h3>
 
       </div>
     </div>
