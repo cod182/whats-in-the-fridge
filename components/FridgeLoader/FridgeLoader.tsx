0dfbf593300ca6735045d01a34a7c3986ac6ed22
Codie Stephens-Evans <codie18@gmail.com>
2024-07-08 13:32:31 +0100

    loader update

--- components/FridgeLoader/FridgeLoader.tsx
+++ components/FridgeLoader/FridgeLoader.tsx
@@ -1,11 +1,19 @@
+'use client'
+
 import Image from "next/image"
+import { Triangle } from 'react-loader-spinner'
 import fridge from '../../public/assets/images/fridge.webp'
 
 const FridgeLoader = () => {
   return (
     <>
-      <Image src={fridge} alt='loading fridge' height={346} width={200} className="animate-pulse" />
-      <p className="font-semibold animate-bounce">Loading...</p>
+      <Triangle height="200"
+        width="200"
+        color="#fff"
+        ariaLabel="loading"
+      />
+      {/* <Image src={fridge} alt='loading fridge' height={346} width={200} className="animate-pulse" /> */}
+      <p className="font-semibold animate-bounce text-white mt-4">Loading...</p>
     </>
   )
 }
