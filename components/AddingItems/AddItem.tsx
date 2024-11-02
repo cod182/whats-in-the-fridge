5f6a966d191b84bc5615b7a60c233aae2be6f79a
Codie Stephens-Evans <codie18@gmail.com>
2024-11-02 20:43:01 +0000

    fix mobile

--- components/AddingItems/AddItem.tsx
+++ components/AddingItems/AddItem.tsx
@@ -107,7 +107,7 @@ const AddItem = ({ selectedArea, availableItems, userCreatedItems, userId, updat
 
           {/* Button for user to create their own item */}
           {/* Only availbe if on mobile */}
-          {!isMobile && (
+          {isMobile && (
             <button onClick={() => setAddType('scan')}
               className={`${addType === 'scan' ? ' border-green-300 border-2' : 'hover:translate-y-1'} flex flex-col items-center justify-center px-2 font-normal bg-from-blue-400 bg-gradient-to-br to-blue-400 from-blue-600 hover:translate-y-[-10px] active:bg-blue-400 aspect-square h-[100px] rounded-full transition-all duration-200 ease`}>
               {/* <Image src='/assets/images/custom.svg' alt='pre-made icon' width={30} height={30} /> */}
